ca51b5c331545cffb71b7a8f54f3acef33bccf99
Maulana Akbar <67249292+maulanaakbardj@users.noreply.github.com>
2024-01-14 10:29:02 +0700

    Update connect.py

--- backend/ai/connect.py
+++ backend/ai/connect.py
@@ -1,18 +1,8 @@
 from backend.env import config
 import requests
-MINDSDB_UN=config("MINDSDB_UN", default=None)# martyna@mindsdb.com
-MINDSDB_PW=config("MINDSDB_PW", default=None) # xxxxxxxx
-
-def init_mindsdb_session():
-    session = requests.Session()
-    session.post('https://cloud.mindsdb.com/cloud/login', json={
-        'email': MINDSDB_UN,
-        'password': MINDSDB_PW
-    })
-    return session
 
 def get_mindsdb_session():
-    session = init_mindsdb_session()
+    session = 'http://127.0.0.1:47334/api/sql/query'
     try:
         yield session
     finally:
