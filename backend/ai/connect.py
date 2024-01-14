cd93cd7c3f1c7a96da99629a7b0c992a0f0a0b9d
Maulana Akbar <67249292+maulanaakbardj@users.noreply.github.com>
2024-01-14 10:30:09 +0700

    Update connect.py

--- backend/ai/connect.py
+++ backend/ai/connect.py
@@ -2,7 +2,7 @@ from backend.env import config
 import requests
 
 def get_mindsdb_session():
-    session = 'http://127.0.0.1:47334/api/sql/query'
+    session = 'http://127.0.0.1:47334'
     try:
         yield session
     finally:
