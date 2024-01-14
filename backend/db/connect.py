626ceda7e1885f0b88033e68f779c521aea44ce4
Maulana Akbar <67249292+maulanaakbardj@users.noreply.github.com>
2024-01-14 10:31:00 +0700

    Update connect.py

--- backend/db/connect.py
+++ backend/db/connect.py
@@ -2,6 +2,15 @@ from backend.env import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+db_config = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': 'password',
+    'database': 'Test'
+}
+
+FLIGHT_PRICES_DATABASE_URL = f"mariadb+mariadbconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
 DATABASE_URL = config("FLIGHT_PRICES_DATABASE_URL", cast=str, default=None)
 DATABASE_URL is not None
 
